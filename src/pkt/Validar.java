48e523c8f6d257fd0cb39269b20ba17855d07176
Usuario <Usuario@LAPTOP-HQ6AESQV>
2023-05-17 12:47:18 +0200

    Modificado por Germán

--- src/pkt/Validar.java
+++ src/pkt/Validar.java
@@ -5,38 +5,34 @@ public class Validar {
 	public static boolean validarString(String cadena, int longitudMax) {
 		return (!cadena.isBlank() && cadena.length() < longitudMax);
 	}
-	
-	
-	
-	public static boolean validarUnidades (int unidades) {
-		return (unidades>=1)? true:false;
+
+	public static boolean validarUnidades(int unidades) {
+		return (unidades >= 1) ? true : false;
 	}
-	
-	
+
 	public static Categorias chkCategorias(String eCate) {
-		char letra=' ';
+		char letra = ' ';
 		Categorias cat = null;
-		
-		if(eCate.length()==1) {
-			letra=eCate.charAt(0);
+
+		if (eCate.length() == 1) {
+			letra = eCate.charAt(0);
 		}
-		
-		if(eCate.equals(Categorias.CATA.getDesc().toUpperCase().replace("Í", "I"))
-				|| letra == '1' || letra == 'A') {
-				cat = Categorias.CATA;
-				
-			}else if(eCate.equals(Categorias.CATB.getDesc().toUpperCase().replace("Í", "I")) 
-				|| letra == '2' || letra == 'B') {
-				cat = Categorias.CATB;
-				
-			}else if(eCate.equals(Categorias.CATC.getDesc().toUpperCase().replace("Í", "I")) 
-				|| letra == '3' || letra == 'C') {
-				cat = Categorias.CATC;
-				
-			}else {
-				System.out.println("Categoria no encontrada");
-			}
-		
+
+		if (eCate.equals(Categorias.CATA.getDesc().toUpperCase().replace("Í", "I")) || letra == '1' || letra == 'A') {
+			cat = Categorias.CATA;
+
+		} else if (eCate.equals(Categorias.CATB.getDesc().toUpperCase().replace("Í", "I")) || letra == '2'
+				|| letra == 'B') {
+			cat = Categorias.CATB;
+
+		} else if (eCate.equals(Categorias.CATC.getDesc().toUpperCase().replace("Í", "I")) || letra == '3'
+				|| letra == 'C') {
+			cat = Categorias.CATC;
+
+		} else {
+			System.out.println("Categoria no encontrada");
+		}
+
 		return cat;
 	}
 }
